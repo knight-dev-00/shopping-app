ce82654babcd2c0bc86b24fb6d26569f2e041044
knight-dev-00 <king.abdelrahman00@gmail.com>
2023-11-09 20:33:39 +0200

    store 1st version

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -2,6 +2,16 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    const { /*expenses, */Location, CartValue } = useContext(AppContext);
+    /*const totalExpenses = expenses.reduce((total, item) => {
+        return (total += (item.unitprice * item.quantity));
+    }, 0);*/
+
+    return (
+        <div className='alert alert-primary'>
+            <span>Cart Value: {Location}{CartValue}</span>
+        </div>
+    );
 };
 
 export default CartValue;
